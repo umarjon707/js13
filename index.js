a2784bd682e85d95682c7e587550e4a1b2379331
Umarjon <urr777777@gmail.com>
2024-04-27 16:35:06 +0500

    all done

--- index.js
+++ index.js
@@ -1,107 +1,25 @@
-let arr = [
-    {
-        "userId" : 1,
-        "id" : 1 ,
-        "title" : "fgeafg",
-        "completed" : false
-    },
-    {
-        "userId" : 1,
-        "id" : 2 ,
-        "title" : "fgeafg",
-        "completed" : false
-    },
-    {
-        "userId" : 1,
-        "id" : 3 ,
-        "title" : "fgeafg",
-        "completed" : false
-    },
-    {
-        "userId" : 1,
-        "id" : 4 ,
-        "title" : "fgeafg",
-        "completed" : true
-    },
-    {
-        "userId" : 1,
-        "id" : 5 ,
-        "title" : "fgeafg",
-        "completed" : false
-    },
-    {
-        "userId" : 1,
-        "id" : 6 ,
-        "title" : "fgeafg",
-        "completed" : false
-    },
-    {
-        "userId" : 1,
-        "id" : 7 ,
-        "title" : "fgeafg",
-        "completed" : true
-    },
-    {
-        "userId" : 1,
-        "id" : 8 ,
-        "title" : "fgeafg",
-        "completed" : true
-    },
-    {
-        "userId" : 1,
-        "id" : 9 ,
-        "title" : "fgeafg",
-        "completed" : false
-    },
-    {
-        "userId" : 1,
-        "id" : 10 ,
-        "title" : "fgeafg",
-        "completed" : true
-    }
 
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let alphabet_reversed = "";
+
+let alphabet_arr = [
+    {
+        idx: 1,
+        leter: "A"
+    }
 ]
 
-let a = {
-    count :0 ,
-    arr : []
-}
-let b = {
-    count :0 ,
-    arr : []
+for (let i = 0; i < alphabet.length; i++) {
+    alphabet_arr.push({
+        idx: i + 1,
+        leter: alphabet[i]
+    });
 }
 
-
-for (let item of arr) {
-    if (item.completed) {
-        b.arr.push(item);
-        b.count++;
-    } else {
-        a.arr.push(item);
-        a.count++;
-    }
+for (let i = alphabet.length - 1; i >= 0; i--) {
+    alphabet_reversed += alphabet[i];
 }
-console.log(
-    a,b
-    );
 
+console.log(alphabet_arr);
+console.log(alphabet_reversed);
 
-
-
-let pwd  = prompt('password?')
-let attemp = 3
-do {
-    pwd  =  +prompt('password?')
-    if (pwd !==7777 ) {
-        alert(attemp--)
-    }  else if (attemp <=0){
-        prompt('dont write')
-    }
-} while (pwd !== 7777 && attemp > 0  ) {
-    alert('good')
-} 
-if (pwd ===7777) {
-    alert('ggood')
- } else {
-    alert('no attemp')
- }
